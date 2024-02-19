46710b13c30c07b5bc4ae2971f8f938997891f10
qingshuang.ye <yeqingshuan@tcsc.com>
2024-02-19 17:45:47 +0800

    15_watch监视_情况三

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,15 +5,25 @@
       <button @click="changeName">修改姓名</button>
       <button @click="changeAge">修改年龄</button>
       <button @click="changePerson">修改整个人</button>
+      <hr>
+      <h2>测试：{{obj.a.b.c}}</h2>
+      <button @click="changeObj">修改值</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-  import {ref,watch} from 'vue'
+  import {reactive,watch} from 'vue'
 
-  let person = ref({
+  let person = reactive({
     name: '张三',
     age: 18
+  })
+  let obj = reactive({
+    a:{
+      b:{
+        c:666
+      }
+    }
   })
 
 /*   监视，情况二：监视【ref】定义的【对象类型】数据，监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启深度监听
@@ -22,16 +32,23 @@
   watch的第三个参数是：配置对象（deep、immediate等等） */
   watch(person,(newValue,oldValue)=>{
     console.log('watch',newValue,oldValue)
-  },{deep:true})
+  })
+  watch(obj,(newValue,oldValue)=>{
+    console.log('watch',newValue,oldValue)
+  })
 
   function changeName(){
-    person.value.name += '~'
+    person.name += '~'
   }
   function changeAge(){
-    person.value.age += 1
+    person.age += 1
   }
   function changePerson(){
-    person.value = {name: '李四', age: 90}
+    // person = {name: '李四', age: 90}
+    Object.assign(person,{name: '李四', age: 90})
+  }
+  function changeObj(){
+    obj.a.b.c = 888
   }
 </script>
 
