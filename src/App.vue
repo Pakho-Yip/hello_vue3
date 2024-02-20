af99d2d744add6716cc17e75b73776f2113adc28
qingshuang.ye <yeqingshuan@tcsc.com>
2024-02-20 18:00:44 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-    <h2 class="person" ref="title2">你好</h2>
-    <button @click="showLog">点我输出h2这个元素</button>
-    <Person ref="ren" />
+    <Person />
 </template>
 
 <script lang="ts" setup name="App">
   import Person from './components/Person.vue'
-  import {ref} from 'vue'
-
-  let title2 = ref()
-  let ren = ref()
-
-  function showLog(){
-    // console.log(document.getElementById('title2'))
-    // console.log(title2.value)
-    console.log(ren.value)
-
-  }
 </script>
 
 <style>
