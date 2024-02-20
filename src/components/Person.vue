79b3fde6143fede8291ce4c45aa0ff3e8b240569
qingshuang.ye <yeqingshuan@tcsc.com>
2024-02-20 11:20:03 +0800

    16_watch监视_情况四

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,12 +2,12 @@
     <div class="person">
       <h2>姓名：{{person.name}}</h2>
       <h2>年龄：{{person.age}}</h2>
+      <h2>汽车：{{person.car.car1}}、{{person.car.car2}}</h2>
       <button @click="changeName">修改姓名</button>
       <button @click="changeAge">修改年龄</button>
-      <button @click="changePerson">修改整个人</button>
-      <hr>
-      <h2>测试：{{obj.a.b.c}}</h2>
-      <button @click="changeObj">修改值</button>
+      <button @click="changeCar1">修改第一辆车</button>
+      <button @click="changeCar2">修改第二辆车</button>
+      <button @click="changeCar">修改整个车</button>
     </div>
 </template>
 
@@ -16,23 +16,22 @@
 
   let person = reactive({
     name: '张三',
-    age: 18
-  })
-  let obj = reactive({
-    a:{
-      b:{
-        c:666
-      }
+    age: 18,
+    car:{
+      car1:'奔驰',
+      car2:'宝马'
     }
   })
 
-  // 监视，情况三：监视【reactive】定义的【对象类型】数据，且默认是开启深度监视的
-  watch(person,(newValue,oldValue)=>{
+  // 监视，情况四：监视响应式对象中的某个属性，且该属性是基本类型的，要写成函数式
+/*   watch(()=>person.name,(newValue,oldValue)=>{
     console.log('watch',newValue,oldValue)
-  })
-  watch(obj,(newValue,oldValue)=>{
+  }) */
+
+  // 监视，情况四：监视响应式对象中的某个属性，且该属性是对象类型的，可以直接写，也能写函数，更推荐写函数
+  watch(()=>person.car,(newValue,oldValue)=>{
     console.log('watch',newValue,oldValue)
-  })
+  },{deep:true})
 
   function changeName(){
     person.name += '~'
@@ -40,12 +39,14 @@
   function changeAge(){
     person.age += 1
   }
-  function changePerson(){
-    // person = {name: '李四', age: 90}
-    Object.assign(person,{name: '李四', age: 90})
+  function changeCar1(){
+    person.car.car1 = '奥迪'
   }
-  function changeObj(){
-    obj.a.b.c = 888
+  function changeCar2(){
+    person.car.car2 = '路虎'
+  }
+  function changeCar(){
+    person.car = {car1:'爱玛',car2:'雅迪'}
   }
 </script>
 
