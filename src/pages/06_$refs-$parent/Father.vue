fb8ad6ec2f994a5883540e429b0faf626327c1e5
Pakho Yip <943389417@qq.com>
2024-03-06 23:51:04 +0800

    50_组件通信_方式6_$refs与$parent

--- src/pages/06_$refs-$parent/Father.vue
+++ src/pages/06_$refs-$parent/Father.vue
@@ -1,14 +1,40 @@
 <template>
 	<div class="father">
 		<h3>父组件</h3>
-		<Child1 />
-		<Child2 />
+		<h4>房产：{{house}}</h4>
+		<button @click="changeToy">修改Child1的玩具</button>
+		<button @click="changeComputer">修改Child2的电脑</button>
+		<button @click="getAllChild($refs)">让所有的孩子书变多</button>
+		<Child1 ref="c1"/>
+		<Child2 ref="c2"/>
 	</div>
 </template>
 
 <script setup lang="ts" name="Father">
 	import Child1 from './Child1.vue'
 	import Child2 from './Child2.vue'
+	import {ref} from 'vue'
+
+	let house = ref(4)
+	let c1 = ref()
+	let c2 = ref()
+
+
+	function changeToy(){
+		console.log(c1.value);
+		c1.value.toy = '小猪佩奇'
+	}
+	function changeComputer(){
+		c2.value.computer = '华为'
+	}
+	function getAllChild(refs:{[key:string]:any}){
+		console.log(refs);
+		for (let key in refs) {
+			refs[key].book += 3
+		}
+	}
+	// 把数据交给外部
+	defineExpose({house})
 </script>
 
 <style scoped>
