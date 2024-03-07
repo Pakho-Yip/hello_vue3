9e8ba22b7ba7a1be7896d1a13fd7aabd0af2b06c
Pakho Yip <943389417@qq.com>
2024-03-07 22:58:27 +0800

    54_插槽_具名插槽

--- src/pages/09_slot/Category.vue
+++ src/pages/09_slot/Category.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="category">
-      <h2>{{title}}</h2>
-      <slot>默认内容</slot>
+      <slot name="s1">默认内容1</slot>
+      <slot name="s2">默认内容2</slot>
     </div>
 </template>
   
 <script setup lang="ts" name="Category">
-    defineProps(['title'])
 </script>
 
 <style scoped>
@@ -17,11 +16,5 @@
     border-radius: 10px;
     box-shadow: 0 0 10px;
     padding: 10px;
-  }
-  h2 {
-    background-color: orange;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 800;
   }
 </style>
--- src/pages/09_slot/Father.vue
+++ src/pages/09_slot/Father.vue
@@ -2,16 +2,31 @@
   <div class="father">
     <h3>父组件</h3>
     <div class="content">
-      <Category title="热门游戏列表">
-        <ul>
-          <li v-for="item in games" :key="item.id">{{ item.name }}</li>
-        </ul>
+      <Category>
+        <template v-slot:s2>
+          <ul>
+            <li v-for="item in games" :key="item.id">{{ item.name }}</li>
+          </ul>
+        </template>
+        <template v-slot:s1>
+          <h2>热门游戏列表</h2>
+        </template>
       </Category>
-      <Category title="今日美食城市">
-        <img :src="imgUrl" alt=""/>
+      <Category>
+        <template v-slot:s2>
+          <img :src="imgUrl" alt=""/>
+        </template>
+        <template v-slot:s1>
+          <h2>今日美食城市</h2>
+        </template>
       </Category>
-      <Category title="今日影视推荐">
-        <video :src="videoUrl" controls></video>
+      <Category>
+        <template #s2>
+          <video :src="videoUrl" controls></video>
+        </template>
+        <template #s1>
+          <h2>今日影视推荐</h2>
+        </template>
       </Category>
     </div>
   </div>
@@ -43,5 +58,11 @@
 }
 img,video {
     width: 100%;
+}
+h2 {
+    background-color: orange;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 800;
 }
 </style>
